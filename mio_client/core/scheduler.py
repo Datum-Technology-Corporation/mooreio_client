716c70a446fa7513f08c792351f618d335e4f98f
David Poulin <dpoulin@datumtc.ca>
2024-10-02 15:38:52 -0400

    Committing scheduler.py code before generating docs with AI

--- mio_client/core/scheduler.py
+++ mio_client/core/scheduler.py
@@ -25,6 +25,8 @@ class Task:
         self._timestamp_start = None
         self._timestamp_end = None
         self._hostname = None
+        self._is_part_of_set = False
+        self._parent_set = None
     
     @property
     def command(self) -> Command:
@@ -73,17 +75,50 @@ class Task:
     @hostname.setter
     def hostname(self, value):
         self._hostname = value
+
+    @property
+    def is_part_of_set(self) -> bool:
+        return self._is_part_of_set
+
+    @property
+    def parent_set(self):
+        return self._parent_set
+    @parent_set.setter
+    def parent_set(self, value):
+        if value is not None:
+            self._is_part_of_set = True
+        self._parent_set = value
     
 
-
 class TaskSet:
-    def __init__(self, rmh: RootManager):
+    def __init__(self, rmh: RootManager, command: Command, name: str):
         self._rmh = rmh
+        self._command = command
+        self._name = name
+        self._tasks = []
+
+    @property
+    def rmh(self) -> RootManager:
+        return self._rmh
+
+    @property
+    def command(self) -> Command:
+        return self._command
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    def add_task(self, task: Task):
+        task.parent_set = self
+        self._tasks.append(task)
 
 
 class TaskSchedulerConfiguration:
     def __init__(self, rmh: RootManager):
         self._output_to_terminal = False
+        self._max_number_of_parallel_processes = 1
+
     @property
     def output_to_terminal(self) -> bool:
         return self._output_to_terminal
@@ -91,9 +126,26 @@ class TaskSchedulerConfiguration:
     def output_to_terminal(self, value: bool):
         self._output_to_terminal = value
 
+    @property
+    def max_number_of_parallel_processes(self) -> int:
+        return self._max_number_of_parallel_processes
+    @max_number_of_parallel_processes.setter
+    def max_number_of_parallel_processes(self, value: int):
+        self._max_number_of_parallel_processes = value
+
 
 class TaskSchedulerDatabase:
-    pass
+    def __init__(self, rmh: RootManager):
+        self._rmh = rmh
+        self._task_schedulers = []
+    def add_task_scheduler(self, task_scheduler: 'TaskScheduler'):
+        task_scheduler.db = self
+        self._task_schedulers.append(task_scheduler)
+    def get_task_scheduler(self, name: str) -> 'TaskScheduler':
+        for task_scheduler in self._task_schedulers:
+            if task_scheduler.name == name:
+                return task_scheduler
+        raise Exception(f"No Task Scheduler '{name}' exists in the Task Scheduler Database")
 
 
 class TaskScheduler(ABC):
@@ -136,8 +188,6 @@ class TaskScheduler(ABC):
         pass
 
 
-
-
 class LocalProcessScheduler(TaskScheduler):
     def init(self):
         pass
@@ -151,8 +201,10 @@ class LocalProcessScheduler(TaskScheduler):
 
     def dispatch_task_set(self, task_set: TaskSet, configuration: TaskSchedulerConfiguration):
         pass
+        # TODO IMPLEMENT
 
 
+# TODO IMPLEMENT!
 class LsfScheduler(TaskScheduler):
     def init(self):
         pass
@@ -164,6 +216,7 @@ class LsfScheduler(TaskScheduler):
         pass
 
 
+# TODO IMPLEMENT!
 class GridEngineScheduler(TaskScheduler):
     def init(self):
         pass
